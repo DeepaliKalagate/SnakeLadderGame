b00a4bb8f8bdbba61160c54ca827cc764588ffd0
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-16 10:28:27 +0530

    Refactored code and Added looping for reaches winning position

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -2,32 +2,43 @@
 
 echo "Welcome to Snake Ladder Game"
 
+#Constant Vabiables
 PLAYER_ONE=1
+INITIAL_POSITION=0
+WON_POSITION=100
+NO_CHANGE=0
+SNAKE=1
+LADDER=2
+
+#Variables
 position=0
 steps=0
 snakeLadder=0
-noChange=0
-snake=1
-ladder=2
 
 
 function ladderSnake()
 {
-	steps=$((RANDOM%6+1))
-	snakeLadder=$((RANDOM%3))
-	case $snakeLadder in
-	$noChange)
-		position=$position;;
-	$snake)
-		if [ $position -lt $steps ]
-		then
-			position=0
-		else
-			position=$(( $position - $steps ))
-		fi;;
-	$ladder)
-		position=$(( $position + $steps )) ;;
+	while [ $position -le $WON_POSITION ]
+	do
+		steps=$((RANDOM%6+1))
+		snakeLadder=$((RANDOM%3))
+		case $snakeLadder in
+			$NO_CHANGE)
+				echo "Not Playing" ;;
+			$SNAKE)
+				position=$(( $position - $steps )) ;;
+			$LADDER)
+				position=$(( $position + $steps )) ;;
 		esac
+			if [ $position -lt $INITIAL_POSITION ]
+			then
+				position=$INITIAL_POSITION
+			fi
+			if [ $position -eq $WON_POSITION ]
+			then
+				break
+			fi
+	done
 }
 
 ladderSnake
