84f3b697283c961ffa5baa502ef5bc8fa0aad104
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-16 10:05:47 +0530

    Added ladder function for player checks the option of Ladder or snake

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -5,6 +5,30 @@ echo "Welcome to Snake Ladder Game"
 PLAYER_ONE=1
 position=0
 steps=0
+snakeLadder=0
+noChange=0
+snake=1
+ladder=2
 
-steps=$((RANDOM%6 + 1))
+
+function ladderSnake()
+{
+	steps=$((RANDOM%6+1))
+	snakeLadder=$((RANDOM%3))
+	case $snakeLadder in
+	$noChange)
+		position=$position;;
+	$snake)
+		if [ $position -lt $steps ]
+		then
+			position=0
+		else
+			position=$(( $position - $steps ))
+		fi;;
+	$ladder)
+		position=$(( $position + $steps )) ;;
+		esac
+}
+
+ladderSnake
 
